f3418ab8c1b02350bb665d021d74140e76b26cda
Miguel Expósito <exposito_m@cantabria.es>
2017-10-25 23:38:23 +0200

    Corregidos decimales

--- infodir.sh
+++ infodir.sh
@@ -6,20 +6,32 @@ do
 
 # bloque para directorios
 if [[ -d $file ]]; then
-    # se cuentan las entradas en un subdirectorio dado con wordcount
+    # se cuentan las entradas en un subdirectorio dado con wordcount aplicado mediante una tuberia al resultado de ls
     number="$(ls -1 $file | wc -l)"
     echo "$file" "(""$number" "entradas)";
 # bloque para archivos
 else
     # se calcula el tamaño en kilobytes para cada archivo con du
-    size="$(du -k "$file" | cut -f1)"
+    size="$(du -kb "$file" | cut -f1)"
     # en función de su tamaño, se utilizan unos u otros multiplicadores de tamaño
-    if [ $(($size * 1000)) -gt $(( 2**30 ))  ]; then
-        echo "$file" "($(($size/1048576))" "GiB)"
-    elif [ $(($size * 1000)) -gt $(( 2**20 ))  ]; then
-        echo "$file" "($(($size/1024))" "MiB)"
+    # bash no soporta division en coma flotante, con lo que es necesario llamar a una utilidad del sistema
+    # awk esta presente por defecto en la mayor parte de las distribuciones Linux.
+    # con awk se calcula la division entre el multiplicador y se almacena el resultado devuelto por print en una
+    # variable. No se considera necesario especificar el numero de decimales a mostrar, dado que no se especifica.
+
+    if [ $(($size)) -gt $(( 2**30 ))  ]; then
+        value=$(awk -v size=$size 'BEGIN { print size / 1073741824 }')
+        echo "$file" "($value" "GiB)"
+    elif [ $(($size)) -gt $(( 2**20 ))  ]; then
+        value=$(awk -v size=$size 'BEGIN { print size / 1048576 }')
+        echo "$file" "($value" "MiB)"
     else
-        echo "$file" "($(($size))" "KiB)"
+        if [ $(($size/1024)) -eq 0 ]; then
+            echo "$file" "(0.$(($size))" "KiB)"
+        else
+            value=$(awk -v size=$size 'BEGIN { print size / 1024 }')
+            echo "$file" "($value" "KiB)"
+        fi
     fi
 fi
 done
